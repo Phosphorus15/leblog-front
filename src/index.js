1cb44edcaa371152e9a84b6392d5eb06c0552981
Phosphorus15 <steepout@qq.com>
2020-04-10 20:06:22 +0800

    front page structure

--- src/index.js
+++ src/index.js
@@ -6,7 +6,24 @@ import './index.css';
 class FrontPage extends React.Component {
 
     render() {
-        return <div/>
+        return (<div className="container">
+            <header className="header">
+                <h1 className="title">
+                    <a className="logo" href="/">
+                        Welcome to the blogging system
+                    </a>
+                </h1>
+                <nav className="links">
+                    <ul className="hide-links">
+                        <li><a href="./login">Login</a></li>
+                        <li><a href="./register">Register</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main className="main">
+                <h3> Recent Posts : </h3>
+            </main>
+        </div>)
     }
 
 }
