848648acba208096890a2a90a33df2368a04236a
Phosphorus15 <steepout@qq.com>
2020-04-22 17:22:39 +0800

    extract header.js

--- src/form.js
+++ src/form.js
@@ -1,24 +1,6 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import {submit_login, submit_register} from "./network";
-
-class PageHeader extends React.Component {
-    render() {
-        return <header className="header">
-            <h1 className="title">
-                <Link className="logo" to="/">
-                    Welcome to the blogging system
-                </Link>
-            </h1>
-            <nav className="links">
-                <ul className="hide-links">
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/register">Register</Link></li>
-                </ul>
-            </nav>
-        </header>;
-    }
-}
+import PageHeader from "./header";
 
 class RegisterPage extends React.Component {
     render() {
